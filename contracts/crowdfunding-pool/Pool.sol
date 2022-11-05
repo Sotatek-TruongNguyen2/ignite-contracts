de2e8f51e62ccbbe6465dbaf408388b54c099952
Sotatek-LoiNguyen2 <loi.nguyen2@sotatek.com>
2022-11-05 23:40:26 +0700

    buy token

--- contracts/crowdfunding-pool/Pool.sol
+++ contracts/crowdfunding-pool/Pool.sol
@@ -10,32 +10,43 @@ import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 contract Pool is Pausable {
     using SafeERC20 for IERC20;
     using SafeMath for uint;
-    IERC20 public token;
+
+    struct OfferedCurrency {
+        uint rate;
+        uint decimal;
+    }
+
+    IERC20 IDOToken;
     IERC20 purchaseToken;
-    uint price;
+    OfferedCurrency offeredCurrency;
     address superAdmin;
     address newSuperAdmin;
     mapping(address => bool) private admin;
     uint maxPurchaseAmountForNotKYCUser;
     uint maxPurchaseAmountForKYCUser;
     uint maxPurchaseAmountForWhale;
-    uint participationFee;
+    uint participationFeePercentage;
     uint totalRaiseAmount;
     uint whaleProportion;
     // bytes32 whaleRoot;
     // bytes32 KYCUserRoot;
     bytes32 root;
     address feeRecipient;
+    address purchaseTokenRecipient;
+    address redeemIDOTokenRecipient;
     uint whaleOpenTime;
     uint whaleDuration;
     uint communityOpenTime;
     uint communityDuration;
+    uint purchasedAmount;
 
     error NotAnAdmin(address _admin);
     error NotSuperAdmin(address _superAdmin);
     error OverlapOpenTime();
     error NotInList();
     error ExceedMaxPurchaseAmount();
+    error ExceedTotalRaiseAmount();
+    error OutOfTime();
 
     modifier onlyAdmin() {
         if (admin[msg.sender] == false) {
@@ -53,24 +64,33 @@ contract Pool is Pausable {
 
     function initialize(
         address _superAdmin,
+        address _IDOToken,
         address _purchaseToken,
-        uint _participationFee,
+        address _feeRecipient,
+        address _purchaseTokenRecipient,
+        address _redeemIDOTokenRecipient,
+        uint _participationFeePercentage,
         uint _totalRaiseAmount,
         uint _whaleProportion,
         uint _whaleOpenTime,
         uint _whaleDuration,
         uint _communityOpenTime,
         uint _communityDuration,
-        uint _price,
         uint _maxPurchaseAmountForNotKYCUser,
-        uint _maxPurchaseAmountForKYCUser
+        uint _maxPurchaseAmountForKYCUser,
+        uint _rate,
+        uint _decimal
     ) public {
         if (_whaleOpenTime + _whaleDuration > _communityOpenTime) {
             revert OverlapOpenTime();
         }
         superAdmin = _superAdmin;
+        IDOToken = IERC20(_IDOToken);
         purchaseToken = IERC20(_purchaseToken);
-        participationFee = _participationFee;
+        feeRecipient = _feeRecipient;
+        purchaseTokenRecipient = _purchaseTokenRecipient;
+        redeemIDOTokenRecipient = _redeemIDOTokenRecipient;
+        participationFeePercentage = _participationFeePercentage;
         totalRaiseAmount = _totalRaiseAmount;
         whaleProportion = _whaleProportion;
         setAdmin(_superAdmin);
@@ -78,14 +98,15 @@ contract Pool is Pausable {
         whaleDuration = _whaleDuration;
         communityOpenTime = _communityOpenTime;
         communityDuration = _communityDuration;
-        price = _price;
         maxPurchaseAmountForNotKYCUser = _maxPurchaseAmountForNotKYCUser;
         maxPurchaseAmountForKYCUser = _maxPurchaseAmountForKYCUser;
         maxPurchaseAmountForWhale = totalRaiseAmount.div(2);
+        offeredCurrency.rate = _rate;
+        offeredCurrency.decimal = _decimal;
     }
 
-    // function setParticipationFee(uint _participationFee) public {
-    //     participationFee = _participationFee;
+    // function setParticipationFeePercentage(uint _participationFeePercentage) public {
+    //     participationFeePercentage = _participationFeePercentage;
     // }
 
     // function setTotalRaiseAmount(uint _totalRaiseAmount) public {
@@ -109,14 +130,6 @@ contract Pool is Pausable {
         newSuperAdmin = address(0);
     }
 
-    // function setWhaleRoot(bytes32 _whalerRoot) public onlyAdmin{
-    //     whaleRoot = _whalerRoot;
-    // }
-
-    // function setKYCUserRoot(bytes32 _KYCUserRoot) public onlyAdmin{
-    //     KYCUserRoot = _KYCUserRoot;
-    // }
-
     function setRoot(bytes32 _root) public {
         root = _root;
     }
@@ -130,29 +143,70 @@ contract Pool is Pausable {
     }
 
     function buyToken(bytes32[] memory proof, uint _purchaseAmount) public {
-        // leaf = {address + max purchase amount}
+        if (purchasedAmount + _purchaseAmount > totalRaiseAmount) {
+            revert ExceedTotalRaiseAmount();
+        }
         if (isInWhaleSession()) {
-            if(_purchaseAmount >= maxPurchaseAmountForWhale){
+            if (purchasedAmount + _purchaseAmount > maxPurchaseAmountForWhale) {
                 revert ExceedMaxPurchaseAmount();
             }
-            if (_verifyUser(msg.sender, maxPurchaseAmountForWhale, proof) == false) {
+            if (
+                _verifyUser(msg.sender, maxPurchaseAmountForWhale, proof) ==
+                false
+            ) {
                 revert NotInList();
             }
-        }
-        if (isInCommunitySession()){
-            if(_purchaseAmount >= maxPurchaseAmountForKYCUser){
+
+            // buy IDO Token
+            _buyIDOToken(msg.sender, _purchaseAmount);
+        } else if (isInCommunitySession()) {
+            if (_purchaseAmount >= maxPurchaseAmountForKYCUser) {
                 revert ExceedMaxPurchaseAmount();
             }
-            if(_purchaseAmount >= maxPurchaseAmountForNotKYCUser){
-                if(_verifyUser(msg.sender, maxPurchaseAmountForKYCUser, proof) == false){
+            if (_purchaseAmount >= maxPurchaseAmountForNotKYCUser) {
+                if (
+                    _verifyUser(
+                        msg.sender,
+                        maxPurchaseAmountForKYCUser,
+                        proof
+                    ) == false
+                ) {
                     revert NotInList();
                 }
             }
-        }
-        uint tokenAmount = _purchaseAmount.div(price);
-        purchaseToken.safeTransferFrom(msg.sender, feeRecipient, _purchaseAmount);
-        token.safeTransfer(msg.sender, tokenAmount);
 
+            // buy IDO Token
+            _buyIDOToken(msg.sender, _purchaseAmount);
+        } else {
+            revert OutOfTime();
+        }
+
+        if (purchasedAmount == totalRaiseAmount) {
+            _pause();
+        }
+    }
+
+    function _buyIDOToken(address buyer, uint _purchaseAmount) public {
+
+        // _purchaseAmount do not include participantFee
+        // calculate participantion fee and transfer to fee recipient
+        uint participationFee = _purchaseAmount
+            .mul(participationFeePercentage)
+            .div(10000);
+        purchaseToken.safeTransferFrom(buyer, feeRecipient, participationFee);
+
+        // buy IDO token
+        purchasedAmount = purchasedAmount.add(_purchaseAmount);
+        purchaseToken.safeTransferFrom(
+            buyer,
+            purchaseTokenRecipient,
+            _purchaseAmount
+        );
+
+        uint IDOTokenAmount = _purchaseAmount.mul(offeredCurrency.rate).div(
+            10**offeredCurrency.decimal
+        );
+        IDOToken.safeTransfer(buyer, IDOTokenAmount);
     }
 
     function isInWhaleSession() public view returns (bool) {
@@ -172,9 +226,16 @@ contract Pool is Pausable {
         uint _maxPurchaseAmount,
         bytes32[] memory proof
     ) public view returns (bool) {
+        // leaf = {address + max purchase amount}
+
         bytes32 leaf = keccak256(
             abi.encodePacked(_candidate, _maxPurchaseAmount)
         );
         return MerkleProof.verify(proof, root, leaf);
+    }
+
+    function redeemIDOToken() public {
+        uint remainAmount = IDOToken.balanceOf(address(this));
+        IDOToken.safeTransfer(redeemIDOTokenRecipient, remainAmount);
     }
 }
